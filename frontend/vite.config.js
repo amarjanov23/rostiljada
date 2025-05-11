8db318eacca38610024bae2a6717748cc07de18e
OGADAMo <adam.marjanovic2@gmail.com>
2025-05-11 16:49:21 +0200

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
@@ -10,7 +9,7 @@ export default defineConfig({
   ],
   server: {
     proxy: {
-      '/api': 'http://localhost:5001',  // Proxy API zahtjeva prema backendu
+      '/api': 'http://localhost:5001',
     },
   },
 });
